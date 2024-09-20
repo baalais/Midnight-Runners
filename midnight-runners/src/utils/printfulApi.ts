6f8fd9acf2937ddc75424b617dd14ab51c3a825f
baalais <99286765+baalais@users.noreply.github.com>
2024-09-20 11:45:27 +0300

    fdc
    fdc

--- midnight-runners/src/utils/printfulApi.ts
+++ midnight-runners/src/utils/printfulApi.ts
@@ -1,40 +1,40 @@
-import fetch from 'node-fetch';
-require('dotenv').config();
+// import fetch from 'node-fetch';
+// require('dotenv').config();
 
-const API_URL = 'https://api.printful.com'; // Base URL for Printful API
-const TOKEN = process.env.PRINTFUL_PRIVATE_TOKEN;
+// const API_URL = 'https://api.printful.com'; // Base URL for Printful API
+// const TOKEN = process.env.PRINTFUL_PRIVATE_TOKEN;
 
-export const fetchPrintfulData = async (endpoint: string): Promise<PrintfulResponse> => {
-  try {
-    const response = await fetch(`${API_URL}${endpoint}`, {
-      method: 'GET',
-      headers: {
-        'Authorization': `Bearer ${TOKEN}`,
-        'Content-Type': 'application/json',
-      },
-    });
+// export const fetchPrintfulData = async (endpoint: string): Promise<PrintfulResponse> => {
+//   try {
+//     const response = await fetch(`${API_URL}${endpoint}`, {
+//       method: 'GET',
+//       headers: {
+//         'Authorization': `Bearer ${TOKEN}`,
+//         'Content-Type': 'application/json',
+//       },
+//     });
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! Status: ${response.status}`);
+//     }
 
-    const data: PrintfulResponse = await response.json();
-    return data;
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    throw error;
-  }
-};
+//     const data: PrintfulResponse = await response.json();
+//     return data;
+//   } catch (error) {
+//     console.error('Error fetching data:', error);
+//     throw error;
+//   }
+// };
 
 
-interface PrintfulProduct {
-    id: number;
-    name: string;
-    // Add other fields as needed
-  }
+// export type PrintfulProduct =  {
+//     id: number;
+//     name: string;
+//     // Add other fields as needed
+//   }
   
-  interface PrintfulResponse {
-    result: PrintfulProduct[];
-    // Add other fields as needed
-  }
+// type PrintfulResponse = {
+//     result: PrintfulProduct[];
+//     // Add other fields as needed
+// }
   
